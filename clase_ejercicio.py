d51a60ba22c1dc2037dc80e86b4abdae08604cda
Selix2002 <150069336+Selix2002@users.noreply.github.com>
2025-03-31 21:29:23 -0300

    Update clase_ejercicio.py
    Ultimo comiite

--- clase_ejercicio.py
+++ clase_ejercicio.py
@@ -1,17 +1,20 @@
 from dataclasses import dataclass
+from dataclasses import dataclass, field
+
 
 @dataclass
 class Libro:
-    title = str
-    autor = str
-    isbn = str
-    disponible = bool 
+    title: str
+    autor: str
+    isbn: str
+    disponible: bool = True
+
     
     def mostrar_info(self):
         if self.disponible:
-            return f"Autor:{self.autor} ; Titulo:{self.title} ; Estado: Disponible"
+            return print(f"Autor:{self.autor} ; Titulo:{self.title} ; Estado: Disponible")
         else:
-            return f"Autor:{self.autor} ; Titulo:{self.title} ; Estado: No disponible"
+            return print(f"Autor:{self.autor} ; Titulo:{self.title} ; Estado: No disponible")
     def prestar(self):
         if self.disponible:
             self.disponible = False
@@ -20,40 +23,41 @@ class Libro:
             self.disponible = True
 @dataclass
 class Usuario:
-    nombre = str
-    id_usuario = str
-    prestamos = list[Libro]
+    nombre : str = ""
+    id_usuario : str = ""
+    prestamos: list[Libro] = field(default_factory=list)
 
     def tomar_libro(self, other: "Libro"):
         if other.disponible:
             self.prestamos.append(other)
         else:
-            return "El libro no esta disponible"
+            return print("El libro no esta disponible")
     def devolver(self, other: "Libro"):
         if other in self.prestamos:
             self.prestamos.remove(other)
             other.disponible = True
         else:
-            return "Este usuario no tiene el libro especificado"
+            return print("Este usuario no tiene el libro especificado")
 
 @dataclass
 class Biblioteca:
-    nombre = str
-    libros = list[Libro]
-    usuarios = list[Usuario]
+    nombre: str = ""
+    libros: list[Libro] = field(default_factory=list)
+    usuarios: list[Usuario] = field(default_factory=list)
+
 
     def agregar_bibloteca(self):
         self.nombre = input("Agregue una biblioteca: ")
     def agregar_libro(self):
-        titulo = input("Ingrese el título del libro: ").strip().lower()
-        autor = input("Ingrese el nombre del autor: ").strip()
-        isbn = input("Ingrese el ISBN del libro: ").strip()
+        titulo = input("Ingrese el título del libro: ")
+        autor = input("Ingrese el nombre del autor: ")
+        isbn = input("Ingrese el ISBN del libro: ")
         
         if titulo in self.libros:
             print("El libro ya existe en la biblioteca.")
             return
 
-        libro = Libro(titulo=titulo, autor=autor, isbn=isbn)
+        libro = Libro(title=titulo, autor=autor, isbn=isbn)
         self.libros.append(libro)
         print(f"Libro '{titulo}' agregado correctamente.")
 
@@ -67,9 +71,12 @@ class Biblioteca:
         for libro in self.libros:
             print(self.libros[libro])
     def buscar_libro_por_titulo(self, titulo: str) -> "Libro | None":
-        return self.libros.get(titulo.lower())
-
+        for libro in self.libros:
+            if libro.title.lower() == titulo.lower():
+                return libro.mostrar_info()
+        return print("NO CACHO BROTHER")  
 
 biblio1 = Biblioteca()
 biblio1.agregar_bibloteca()
 biblio1.agregar_libro()
+biblio1.buscar_libro_por_titulo("aaa")
